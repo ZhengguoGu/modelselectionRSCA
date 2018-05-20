2638e5bb60059fd35941416564dbde6128ff44a1
Zhengguo Desktop Home <z.gu@uvt.nl>
2018-05-20 17:05:05 +0200

    new model selection method Bolasso

---
+++ BolassoCV.R
@@ -0,0 +1,72 @@
+#Bolasso with CV
+
+# Input parameters:
+# DATA: Concatenated data matrix (!!! Not standardized! the matrix will be standardized after each resampling)
+# Jk: A vector. Each element of this vector is the number of columns of a data block
+# N_boots: number of bootstrap relicates
+# R: The number of components (R>=2)
+# LassoSequence: A vector of lasso tuning parameter values in accending order
+# GlassoSequence: A vector of Group Lasso tuning parameter values in accending order
+# w: whether the data should be weighted by its number of variables; If yes, then w=TRUE. This is used because after each resampling, the data needs to be standardized by pre_process(), a function from the RegularizedSCA package. 
+# NRSTARTS: Number of multistarts
+BolassoCV <- function(DATA, Jk, R, LassoSequence, GlassoSequence, W, NRSTARTS){
+  
+  n_persons <- nrow(DATA)
+  
+  person_index <- sample(1:n_persons, n_persons, replace = TRUE)
+  Data_sample <- DATA[person_index, ]
+  Data_sample <- RegularizedSCA::pre_process(Data_sample, weight = w)
+  result <- cv_sparseSCA(Data_sample, Jk, R, LassoSequence, GLassoSequence)  
+  T_target <- result$T_hat            #We fix the estimated T matrix from the first resampled data.
+                                      #All the estimated T matrix are to be compared to this estimated T.       
+                                      #(This is due to permutation freedom)
+  P_indexset <- result$P_hat
+  P_indexset[which(P_indexset!=0)] <- 1  #non-zero loadings are marked as 1
+  
+  #resampling
+  i <- 1
+  while(i < m){
+    person_index <- sample(1:n_persons, n_persons, replace = TRUE)
+    Data_sample <- DATA[person_index, ]
+    Data_sample <- RegularizedSCA::pre_process(Data_sample, weight = w)
+    
+    result <- cv_sparseSCA(Data_sample, Jk, R, LassoSequence, GLassoSequence)
+    
+    T_result <- result$T_hat
+    perm <- RegularizedSCA::TuckerCoef(T_target, T_result)$perm
+    P_result <- result$P_hat[, perm]
+    
+    P_result[which(P_result!=0)] <- 1
+    
+    P_indexset <- P_indexset + P_result
+    
+  }
+  
+  # Reestimate P and T
+  Pout3d <- list()
+  Tout3d <- list()
+  LOSS <- array()
+  LOSSvec <- list()
+  
+  for (n in 1:NRSTARTS) {
+    VarSelectResult <- StrucSCA_withIndex(DATA, Jk, R, P_indexset = P_indexset, MaxIter=400)
+    Pout3d[[n]] <- VarSelectResult$Pmatrix
+    Tout3d[[n]] <- VarSelectResult$Tmatrix
+    LOSS[n] <- VarSelectResult$Loss
+    LOSSvec[[n]] <- VarSelectResult$Lossvec
+  }
+  k <- which(LOSS == min(LOSS))
+  if (length(k) > 1) {
+    pos <- sample(1:length(k), 1)
+    k <- k[pos]
+  }
+  
+  PoutBest <- Pout3d[[k]]
+  ToutBest <- Tout3d[[k]]
+  
+  return_varselect <- list()
+  return_varselect$Pmatrix <- PoutBest
+  return_varselect$Tmatrix <- ToutBest
+  
+  return(return_varselect)
+}
---
+++ StrucSCA_withIndex.R
@@ -0,0 +1,58 @@
+StrucSCA_withIndex <- function (DATA, Jk, R, P_indexset, MaxIter) {
+  DATA <- data.matrix(DATA)
+  
+  I_Data <- dim(DATA)[1]
+  sumJk <- dim(DATA)[2]
+  eps <- 10^(-12)
+  if (missing(MaxIter)) {
+    MaxIter <- 400
+  }
+  P <- matrix(stats::rnorm(sumJk * R), nrow = sumJk, ncol = R)
+  P[P_indexset == 0] <- 0
+  Pt <- t(P)
+
+  sqP <- P^2
+  residual <- sum(DATA^2)
+  Lossc <- residual 
+  
+  conv <- 0
+  iter <- 1
+  Lossvec <- array()
+  while (conv == 0) {
+  
+    SVD_DATA <- svd(DATA, R, R)
+    Tmat <- SVD_DATA$u
+    
+    residual <- sum((DATA - Tmat %*% Pt)^2)
+    Lossu <- residual + pen1
+    
+    P <- t(DATA) %*% Tmat
+    P[P_indexset == 0] <- 0
+    Pt <- t(P)
+    
+    sqP <- P^2
+    residual <- sum((DATA - Tmat %*% Pt)^2)
+    Lossu2 <- residual
+    if (abs(Lossc - Lossu) < 10^(-9)) {
+      Loss <- Lossu
+      residual <- residual
+      P[abs(P) <= 2 * eps] <- 0
+      conv <- 1
+    }
+    else if (iter > MaxIter) {
+      Loss <- Lossu
+      residual <- residual
+      P[abs(P) <= 2 * eps] <- 0
+      conv <- 1
+    }
+    Lossvec[iter] <- Lossu
+    iter <- iter + 1
+    Lossc <- Lossu2
+  }
+  return_varselect <- list()
+  return_varselect$Pmatrix <- P
+  return_varselect$Tmatrix <- Tmat
+  return_varselect$Loss <- Loss
+  return_varselect$Lossvec <- Lossvec
+  return(return_varselect)
+}
