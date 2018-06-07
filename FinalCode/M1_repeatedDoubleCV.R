5467a3c96c0981d1f9ffe973ec78cc7d13290cb2
Zhengguo Desktop Home <z.gu@uvt.nl>
2018-06-07 13:15:24 +0200

    update

--- FinalCode/M1_repeatedDoubleCV.R
+++ FinalCode/M1_repeatedDoubleCV.R
@@ -9,36 +9,55 @@
 # N_cores: number of cores (for parallel computing)
 #######################################################################################
 
-set.seed(112)
-library(RegularizedSCA)
-library(foreach)
-library(doSNOW)
-library(doRNG)
+M1_repeatedDoubleCV <- function(DATA, N_cores, Lasso_length, GLasso_length, n_rep, n_seg){
+  library(RegularizedSCA)
+  library(foreach)
+  library(snow)
+  library(doSNOW)
+  library(doRNG)
 
-N_cores <- 2
+  if(missing(N_cores)){
+    N_cores = 2
+  }
+  
+  if(missing(Lasso_length)){
+    Lasso_length = 50
+  }
+  
+  if(missing(GLasso_length)){
+    GLasso_length =50
+  }
 
-DATA <- data.matrix(Data_final)
-nsub <- dim(DATA)[1]
+  if(missing(n_rep)){
+    n_rep <- 2
+  }
+  
+  if(missing(n_seg)){
+    n_seg <- 3
+  }
+ 
+  
+  DATA <- data.matrix(DATA)
+  nsub <- dim(DATA)[1]
 
-LassoSequence = seq(.00001, RegularizedSCA::maxLGlasso(DATA, Jk, R)$Lasso, length.out = 5)
-GLassoSequence = seq(.00001, RegularizedSCA::maxLGlasso(DATA, Jk, R)$Glasso, length.out = 5)
+  LassoSequence = seq(.00001, RegularizedSCA::maxLGlasso(DATA, Jk, R)$Lasso, length.out = Lasso_length)
+  GLassoSequence = seq(.00001, RegularizedSCA::maxLGlasso(DATA, Jk, R)$Glasso, length.out = GLasso_length)
 
-n_rep <- 2
-n_seg <- 3
+
 ################################ Repeated Double CV ###########################################
 
 
 perc_test <- 1/n_seg
 
-cl <- makeCluster(N_cores)
-registerDoSNOW(cl)
+cl <- snow::makeCluster(N_cores)
+doSNOW::registerDoSNOW(cl)
 
 #note that set.seed() and %dorng% ensure that parallel computing generates reproducable results.
-sim_result <- foreach(r = 1:n_rep, .combine='cbind') %dorng% {
+sim_result <- foreach::foreach(r = 1:n_rep, .combine='cbind') %dorng% {
     
     Flag_person <- 0  # this, together with the following while(min_person=0), is to ensure that
     # the number of subject in testset > number of components. !!! important
-    randindex <- runif(nsub, 0, 1)
+    randindex <- stats::runif(nsub, 0, 1)
     testset_indexsize <- array(NA, n_seg) #note that each element in the array will be compared to R.
     while(Flag_person == 0){
       for(i in 1:n_seg){
@@ -47,7 +66,7 @@ sim_result <- foreach(r = 1:n_rep, .combine='cbind') %dorng% {
         testset_indexsize[i] <- sum(testset_index)
       }
       if(sum(testset_indexsize<=R)>0){
-        randindex <- runif(nsub, 0, 1)  #The number of persons should be > than R. if not, resample.
+        randindex <- stats::runif(nsub, 0, 1)  #The number of persons should be > than R. if not, resample.
       }else{
         Flag_person <- 1
       }
@@ -81,12 +100,13 @@ sim_result <- foreach(r = 1:n_rep, .combine='cbind') %dorng% {
     return(final_sim)
 }
 
-stopCluster(cl)
+SNOW::stopCluster(cl)
 
 #results <- list()
 #results$Lasso <- OptimumLasso
 #results$GLasso <- OptimumGLasso
 #results$e_hat <- E_hat
 
-table(OptimumLasso)
-table(OptimumGLasso)
+return_tables <- list(Lasso = table(OptimumLasso), Group Lasso = table(OptimumGLasso))
+return(return_tables)
+}
