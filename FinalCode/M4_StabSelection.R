ad4af39cebbc3b7734d2f985ee0bed72e680d671
Zhengguo Desktop Home <z.gu@uvt.nl>
2018-08-10 10:31:38 +0200

    update

--- FinalCode/M4_StabSelection.R
+++ FinalCode/M4_StabSelection.R
@@ -27,7 +27,7 @@ M4_StabSelection <- function(DATA, Jk, R, LassoSequence, N_loading, Thr, NRSTART
   }
   
   # need a target matrix for T, so that all the estimated T can be compared to it.
-  result <- RegularizedSCA::cv_sparseSCA(DATA, Jk, R, MaxIter = 400, NRSTARTS = 5, LassoSequence, GLassoSequence=0, nfolds = 7, method = "component")  
+  result <- RegularizedSCA::cv_sparseSCA(DATA, Jk, R, MaxIter = 400, NRSTARTS = NRSTARTS, LassoSequence, GLassoSequence=0, nfolds = 7, method = "component")  
   T_target <- result$T_hat                #We fix the estimated T matrix. All the estimated P in the following resampling procedure will be rotated 
                                           #after comparing the estimated T with with T_target.
                                           #Arguably, using CV to generate T_target might not be a good idea. By the time I'm writing this code, I have already
@@ -43,7 +43,7 @@ M4_StabSelection <- function(DATA, Jk, R, LassoSequence, N_loading, Thr, NRSTART
   sim_result <- foreach::foreach(i = 1:100, .combine='+') %dorng% {
     
     person_index <- sample(1:n_persons, n_persons/2, replace = F)
-    result <- RegularizedSCA::sparseSCA(DATA[person_index, ], Jk, R, LASSO = LassoSequence[1], GROUPLASSO = 0, MaxIter = 400, NRSTARTS = 5, method = "component")
+    result <- RegularizedSCA::sparseSCA(DATA[person_index, ], Jk, R, LASSO = LassoSequence[1], GROUPLASSO = 0, MaxIter = 400, NRSTARTS = NRSTARTS, method = "component")
     perm <- RegularizedSCA::TuckerCoef(T_target[person_index, ], result$Tmatrix)$perm
     P_result <- result$Pmatrix[, perm]
     P_result[which(P_result!=0)] <- 1
@@ -62,7 +62,7 @@ M4_StabSelection <- function(DATA, Jk, R, LassoSequence, N_loading, Thr, NRSTART
     sim_result <- foreach::foreach(i = 1:100, .combine='+') %dorng% {
       
       person_index <- sample(1:n_persons, n_persons/2, replace = F)
-      result <- RegularizedSCA::sparseSCA(DATA[person_index, ], Jk, R, LASSO = LassoSequence[j], GROUPLASSO = 0, MaxIter = 400, NRSTARTS = 5, method = "component")
+      result <- RegularizedSCA::sparseSCA(DATA[person_index, ], Jk, R, LASSO = LassoSequence[j], GROUPLASSO = 0, MaxIter = 400, NRSTARTS = NRSTARTS, method = "component")
       perm <- RegularizedSCA::TuckerCoef(T_target[person_index, ], result$Tmatrix)$perm
       P_result <- result$Pmatrix[, perm]
       P_result[which(P_result!=0)] <- 1
--- FinalCode/simulation.R
+++ FinalCode/simulation.R
@@ -94,7 +94,7 @@ num_correct <- function (TargetP, EstimatedP){
 
 ##############################################################################################################
 ####
-#### Simulations: manually change the parameters: PropNoise and Perc0
+#### Simulations
 ####
 ##############################################################################################################
 
@@ -279,7 +279,7 @@ while(n_dataset <= N_dataset){
   
   n_loading <- sum(my_data_list$P_mat !=0) # note! in reality we dont know the number of non-zero loadings! We just want to see if we know n_loading a priori, can the method generates good results?
   
-  LassoSequence = exp(seq(from = log(0.00000001), to = log(RegularizedSCA::maxLGlasso(DATA, Jk, R)$Lasso), length.out = 2500))  #since we use Lasso only
+  LassoSequence = exp(seq(from = log(0.00000001), to = log(RegularizedSCA::maxLGlasso(DATA, Jk, R)$Lasso), length.out = 500))  #we use Lasso only
   result_sim1_StabS <- M4_StabSelection(my_data_list$data, Jk, R, LassoSequence = LassoSequence, N_loading = n_loading, Thr = .6, NRSTARTS, N_cores)
   
   tuckerresult_StabS <- RegularizedSCA::TuckerCoef(my_data_list$T_mat, result_sim1_StabS$T_hat)    
@@ -290,4 +290,4 @@ while(n_dataset <= N_dataset){
   
   n_dataset <- n_dataset + 1
 }
-
+save(RESULT_StabS, ESTIMATED_PStabS, ESTIMATED_TStabS, file = "Stability.RData")
