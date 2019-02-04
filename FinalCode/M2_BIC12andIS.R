11c1a0086506d999c4a280613760da319a1eba6b
Zhengguo MacbookPro <zhengguo.gu@gmail.com>
2019-02-04 11:25:33 +0100

    found an obvious by very important bug thanks to Niek

--- FinalCode/M2_BIC12andIS.R
+++ FinalCode/M2_BIC12andIS.R
@@ -27,11 +27,11 @@ M2_BIC_IS <- function(DATA, Jk, R, LassoSequence, GLassoSequence, NRSTARTS){
   P_hat0 <- VarSelect0$Pmatrix
   T_hat0 <- VarSelect0$Tmatrix
   
-  V_0 <- sum(DATA - T_hat0%*%t(P_hat0))^2  # this is for BIC_Croux and BIC_GUO
+  V_0 <- sum(DATA - T_hat0%*%t(P_hat0)^2)  # this is for BIC_Croux and BIC_GUO
   error_var <- V_0 / n_sub  #this is for BIC_Guo
   
-  V_oo <- sum(DATA)^2  # this is for Index of sparseness (IS)
-  V_s <- sum(T_hat0%*%t(P_hat0))^2  # this is for IS
+  V_oo <- sum(DATA^2)  # this is for Index of sparseness (IS)
+  V_s <- sum(T_hat0%*%t(P_hat0)^2)  # this is for IS
   
   BIC_Croux <- matrix(NA, length(LassoSequence), length(GLassoSequence))
   BIC_Guo <- matrix(NA, length(LassoSequence), length(GLassoSequence))
@@ -42,12 +42,12 @@ M2_BIC_IS <- function(DATA, Jk, R, LassoSequence, GLassoSequence, NRSTARTS){
       VarSelect <- RegularizedSCA::sparseSCA(DATA, Jk, R, LASSO = LassoSequence[i], GROUPLASSO = GLassoSequence[j], MaxIter = 400, NRSTARTS = 5, method = "component")
       P_hat <- VarSelect$Pmatrix
       T_hat <- VarSelect$Tmatrix
-      V_tilde <- sum(DATA - T_hat %*% t(P_hat))^2
+      V_tilde <- sum(DATA - T_hat %*% t(P_hat)^2)
       
       BIC_Croux[i, j] <- V_tilde / V_0 + sum(P_hat != 0) * log(n_sub) / n_sub  # this is the BIC index prop0sed by Croux et al.
       BIC_Guo[i, j] <- V_tilde / error_var + sum(P_hat != 0) * log(n_sub)      # this is the BIC index proposed by Guo et al.
       
-      V_a <- sum(T_hat %*% t(P_hat))^2  # this is for IS
+      V_a <- sum(T_hat %*% t(P_hat)^2)  # this is for IS
       IS[i, j] <- V_a * V_s / V_oo^2 * sum(P_hat == 0) /(sum(Jk) * R)          # this is index of sparseness
     }
   }
