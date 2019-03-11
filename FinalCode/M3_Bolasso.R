587ca7c50735a5a8ebd523126cc83165b373aec1
Zhengguo Desktop Home <z.gu@uvt.nl>
2019-03-11 17:27:50 +0100

    adjust parameters MaxIter and nfolds

--- FinalCode/M3_Bolasso.R
+++ FinalCode/M3_Bolasso.R
@@ -33,7 +33,7 @@ Bolasso_CV <- function(DATA, Jk, R, N_boots, LassoSequence, GLassoSequence, N_co
   n_persons <- nrow(DATA)
   person_index <- sample(1:n_persons, n_persons, replace = TRUE)
   Data_sample <- DATA[person_index, ]
-  result <- RegularizedSCA::cv_sparseSCA(Data_sample, Jk, R, MaxIter = 300, NRSTARTS = NRSTARTS, LassoSequence, GLassoSequence, nfolds = 5, method = "component")  
+  result <- RegularizedSCA::cv_sparseSCA(Data_sample, Jk, R, MaxIter = 400, NRSTARTS = NRSTARTS, LassoSequence, GLassoSequence, nfolds = 7, method = "component")  
   T_target <- result$T_hat                #We fix the estimated T matrix from the first resampled data.
                                           #All the estimated T matrix are to be compared to this estimated T.       
                                           #(This is due to permutation freedom)
@@ -48,7 +48,7 @@ Bolasso_CV <- function(DATA, Jk, R, N_boots, LassoSequence, GLassoSequence, N_co
     
     person_index <- sample(1:n_persons, n_persons, replace = TRUE)
     Data_sample <- DATA[person_index, ]
-    result <- RegularizedSCA::cv_sparseSCA(Data_sample, Jk, R, MaxIter = 300, NRSTARTS = 5, LassoSequence, GLassoSequence, nfolds = 5, method = "component")
+    result <- RegularizedSCA::cv_sparseSCA(Data_sample, Jk, R, MaxIter = 400, NRSTARTS = NRSTARTS, LassoSequence, GLassoSequence, nfolds = 7, method = "component")
     T_result <- result$T_hat
     perm <- RegularizedSCA::TuckerCoef(T_target, T_result)$perm
     P_result <- result$P_hat[, perm]
@@ -70,7 +70,7 @@ Bolasso_CV <- function(DATA, Jk, R, N_boots, LassoSequence, GLassoSequence, N_co
   LOSSvec <- list()
   
   for (n in 1:5) { 
-    VarSelectResult <- StrucSCA_withIndex(DATA, Jk, R, P_indexset = P_indexset, MaxIter=300)
+    VarSelectResult <- StrucSCA_withIndex(DATA, Jk, R, P_indexset = P_indexset, MaxIter=400)
     Pout3d[[n]] <- VarSelectResult$Pmatrix
     Tout3d[[n]] <- VarSelectResult$Tmatrix
     LOSS[n] <- VarSelectResult$Loss
