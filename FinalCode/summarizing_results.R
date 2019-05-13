3bf1bcd74a5121dd19b0c650856e73009b8895a0
Zhengguo MacbookPro <zhengguo.gu@gmail.com>
2019-05-13 18:04:31 +0200

    update

--- FinalCode/summarizing_results.R
+++ FinalCode/summarizing_results.R
@@ -144,3 +144,119 @@ p_tucker <- ggplot(dat_temp, aes(x = variable, y = value)) +
 p_tucker
 ################################################################################################
 
+################ PART 1b: boxplot (2 data blocks), seperately for variable correctly selected and for zeros corrected identified ####################
+# (authors' comment: first we have to record the number of non-zero loadings that are correctly identified
+# and also the number of zero loadings that are correctly identified.)
+
+numNo0_correct <- function(MATa, MATb){
+  num_corr <- sum((MATa != 0) & (MATb != 0))
+  return(num_corr)
+}
+num0_correct <- function(MATa, MATb){
+  num_corr <- sum((MATa == 0) & (MATb == 0))
+  return(num_corr)
+}
+#load("I_20_J1_40_J2_10_benchmark_CV.RData")  !!!! manually load data!
+#load("I_20_J1_40_J2_10_BIC_IS.RData")
+#load("I_20_J1_40_J2_10_RepeatedDCV.RData")
+#load("I_20_J1_40_J2_10_Stability.RData")
+
+
+ratio_nonzero_zero <- function(set_directory){
+  
+  setwd(set_directory)
+  n_dataset <- 1
+  
+  numNo0_true <- array()
+  num0_true <- array()
+  
+  numNo0_crt_Benchmark <- array()   #number of non-zero loadings 
+  num0_Benchmark <- array()
+  numNo0_crt_RdCV <- array() 
+  num0_RdCV <- array()
+  numNo0_crt_BIC <- array() 
+  num0_BIC <- array()
+  numNo0_crt_IS <- array() 
+  num0_IS <- array()
+  numNo0_crt_Bolasso <- array() 
+  num0_Bolasso <- array()
+  numNo0_crt_Stab <- array() 
+  num0_Stab <- array()
+  
+  
+  while(n_dataset <= 20){
+    
+    filename <- paste("Data_", n_dataset, ".RData", sep = "")
+    load(filename)
+    
+    numNo0_true[n_dataset] <- sum(my_data_list$P_mat !=0)
+    num0_true[n_dataset] <- sum(my_data_list$P_mat ==0)
+    
+    #BenchmarkCV
+    tuckerresult <- RegularizedSCA::TuckerCoef(my_data_list$T_mat, ESTIMATED_T[[n_dataset]])
+    numNo0_crt_Benchmark[n_dataset]<- numNo0_correct(ESTIMATED_P[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    num0_Benchmark[n_dataset] <- num0_correct(ESTIMATED_P[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    
+    #RdCV
+    tuckerresult <- RegularizedSCA::TuckerCoef(my_data_list$T_mat, ESTIMATED_TrdCV[[n_dataset]])
+    numNo0_crt_RdCV[n_dataset] <- numNo0_correct(ESTIMATED_PrdCV[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    num0_RdCV[n_dataset]<- num0_correct(ESTIMATED_PrdCV[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    
+    #BIC
+    tuckerresult <- RegularizedSCA::TuckerCoef(my_data_list$T_mat, ESTIMATED_Tbic[[n_dataset]])
+    numNo0_crt_BIC[n_dataset] <- numNo0_correct(ESTIMATED_Pbic[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    num0_BIC[n_dataset] <- num0_correct(ESTIMATED_Pbic[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    
+    #IS
+    tuckerresult <- RegularizedSCA::TuckerCoef(my_data_list$T_mat, ESTIMATED_TIS[[n_dataset]])
+    numNo0_crt_IS[n_dataset] <- numNo0_correct(ESTIMATED_PIS[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    num0_IS[n_dataset] <- num0_correct(ESTIMATED_PIS[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    
+    #Bolasso
+    tuckerresult <- RegularizedSCA::TuckerCoef(my_data_list$T_mat, ESTIMATED_Tbolasso[[n_dataset]])
+    numNo0_crt_Bolasso[n_dataset] <- numNo0_correct(ESTIMATED_Pbolasso[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    num0_Bolasso[n_dataset] <- num0_correct(ESTIMATED_Pbolasso[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    
+    #Stability Selection
+    tuckerresult <- RegularizedSCA::TuckerCoef(my_data_list$T_mat, ESTIMATED_TStabS[[n_dataset]])
+    numNo0_crt_Stab[n_dataset] <- numNo0_correct(ESTIMATED_PStabS[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    num0_Stab[n_dataset] <- num0_correct(ESTIMATED_PStabS[[n_dataset]][, tuckerresult$perm], my_data_list$P_mat )
+    
+    n_dataset <- n_dataset + 1
+  }
+  
+  Ratio_numNo0_crt_Benchmark <- numNo0_crt_Benchmark / numNo0_true 
+  Ratio_num0_Benchmark <- num0_Benchmark / num0_true
+  Ratio_numNo0_crt_RdCV <- numNo0_crt_RdCV / numNo0_true
+  Ratio_num0_RdCV <- num0_RdCV / num0_true
+  Ratio_numNo0_crt_BIC <- numNo0_crt_BIC / numNo0_true
+  Ratio_num0_BIC <- num0_BIC / num0_true
+  Ratio_numNo0_crt_IS <- numNo0_crt_IS / numNo0_true
+  Ratio_num0_IS <- num0_IS / num0_true
+  Ratio_numNo0_crt_Bolasso <- numNo0_crt_Bolasso / numNo0_true
+  Ratio_num0_Bolasso <- num0_Bolasso / num0_true
+  Ratio_numNo0_crt_Stab <- numNo0_crt_Stab / numNo0_true
+  Ratio_num0_Stab <- num0_Stab / num0_true
+  
+  result <- cbind(Ratio_numNo0_crt_Benchmark, Ratio_numNo0_crt_RdCV, Ratio_numNo0_crt_BIC, Ratio_numNo0_crt_IS, Ratio_numNo0_crt_Bolasso, Ratio_numNo0_crt_Stab,
+                  Ratio_num0_Benchmark, Ratio_num0_RdCV, Ratio_num0_BIC, Ratio_num0_IS, Ratio_num0_Bolasso, Ratio_num0_Stab)
+  return(result)
+}
+
+# Sim_1 0.5% noise and 30% zero ######
+#(note: load data by hand)
+set_directory <- "/Users/zhengguo/Dropbox/tilburg office/Research SCA/Project 3 model selection/1reviewSciRep/Simulations/2block_I20_J40_10/0_005noise_0_3zeros"
+result_sim1 <- ratio_nonzero_zero(set_directory)
+save(result_sim1, file = "/Users/zhengguo/Dropbox/tilburg office/Research SCA/Project 3 model selection/1reviewSciRep/Simulations/2block_I20_J40_10/seperate_sim1.RData")
+# Sim_2 0.5% noise and 50% zero ######
+set_directory <- "/Users/zhengguo/Dropbox/tilburg office/Research SCA/Project 3 model selection/1reviewSciRep/Simulations/2block_I20_J40_10/0_005noise_0_5zeros"
+result_sim2 <- ratio_nonzero_zero(set_directory)
+save(result_sim2, file = "/Users/zhengguo/Dropbox/tilburg office/Research SCA/Project 3 model selection/1reviewSciRep/Simulations/2block_I20_J40_10/seperate_sim2.RData")
+# Sim_3 30% noise and 30% zero #######
+set_directory <- "/Users/zhengguo/Dropbox/tilburg office/Research SCA/Project 3 model selection/1reviewSciRep/Simulations/2block_I20_J40_10/0_3noise_0_3zeros"
+result_sim3 <- ratio_nonzero_zero(set_directory)
+save(result_sim3, file = "/Users/zhengguo/Dropbox/tilburg office/Research SCA/Project 3 model selection/1reviewSciRep/Simulations/2block_I20_J40_10/seperate_sim3.RData")
+# Sim_4 30% noise and 50% zero #######
+set_directory <- "/Users/zhengguo/Dropbox/tilburg office/Research SCA/Project 3 model selection/1reviewSciRep/Simulations/2block_I20_J40_10/0_3noise_0_5zeros"
+result_sim4 <- ratio_nonzero_zero(set_directory)
+save(result_sim4, file = "/Users/zhengguo/Dropbox/tilburg office/Research SCA/Project 3 model selection/1reviewSciRep/Simulations/2block_I20_J40_10/seperate_sim4.RData")
