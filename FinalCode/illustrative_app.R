71fced7c9ced44dcaae5f396acabc50a6c7ffc29
Zhengguo Desktop Home <z.gu@uvt.nl>
2019-06-09 14:24:41 +0200

    update

--- FinalCode/illustrative_app.R
+++ FinalCode/illustrative_app.R
@@ -9,7 +9,49 @@ library(snow)
 library(doSNOW)
 library(doRNG)
 
-########## 1. re-analysis of the parent-child relationship survey data
+######### 1. analysis of the herring data
+
+#1) load the package and data, and pre-process the data
+library(RegularizedSCA)
+names(Herring)    #the herring data is included in the package RegularizedSCA
+
+ChemPhy <- pre_process(Herring$Herring_ChemPhy)
+Sensory <- pre_process(Herring$Herring_Sensory)
+herring_data <- cbind(ChemPhy, Sensory)
+num_var <- cbind(dim(ChemPhy)[2], dim(Sensory)[2])
+
+R <- 4 # known based on previous research Gu and Van Deun 2018
+
+Lassosequence <- seq(0.0000001, maxLGlasso(herring_data, num_var, R)$Lasso, length.out = 50)
+GLassosequence <- seq(0.0000001, maxLGlasso(herring_data, num_var, R)$Glasso, length.out = 50)
+
+#2) load function M2_BIC12andIS.R 
+source("M2_BIC12andIS.R")
+
+set.seed(115)
+ptm <- proc.time()
+result_her_BICIS <- M2_BIC_IS(herring_data, num_var, R, LassoSequence = Lassosequence, GLassoSequence = GLassosequence, NRSTARTS=5)
+
+IS_index <- which(result_her_BICIS$IS == max(result_her_BICIS$IS), arr.ind = T)
+Lasso_IS <- max(Lassosequence[IS_index[1]])
+Glasso_IS <- max(GLassosequence[IS_index[2]])
+final_her_IS <- RegularizedSCA::sparseSCA(herring_data, num_var, R, LASSO = Lasso_IS, GROUPLASSO = Glasso_IS, MaxIter = 400, NRSTARTS = 20, method = "component")
+savetime_her_IS <- proc.time() - ptm
+save(final_her_IS, savetime_her_IS, file="her_IS.RData")
+
+
+#4) generate a table 
+load("her_IS.RData")
+
+set.seed(115)
+
+final_her_IS <- undoShrinkage(herring_data, R, 
+                              her_IS_Pmatrix)  #position of components changed so as to be compared to the results by RdCV
+final_her_IS$Pmatrix
+write.table(final_her_IS$Pmatrix, "final_her.csv", sep = ",")
+
+
+########## 2. re-analysis of the parent-child relationship survey data
 #1) load data
 load("family_data.RData")
 
@@ -52,80 +94,7 @@ write.table(final_fam_IS$Pmatrix, "final_fam.csv", sep = ",")
 
 
 
-######### 2. analysis of the herring data
 
-#1) load the package and data, and pre-process the data
-library(RegularizedSCA)
-names(Herring)    #the herring data is included in the package RegularizedSCA
-
-ChemPhy <- pre_process(Herring$Herring_ChemPhy)
-Sensory <- pre_process(Herring$Herring_Sensory)
-herring_data <- cbind(ChemPhy, Sensory)
-num_var <- cbind(dim(ChemPhy)[2], dim(Sensory)[2])
-
-R <- 4 # known based on previous research Gu and Van Deun 2018
-
-Lassosequence <- seq(0.0000001, maxLGlasso(herring_data, num_var, R)$Lasso, length.out = 50)
-GLassosequence <- seq(0.0000001, maxLGlasso(herring_data, num_var, R)$Glasso, length.out = 50)
-
-#2) load function M1_repeatedDoubleCV.R
-source("M1_repeatedDoubleCV.R")
-
-set.seed(115)
-
-ptm <- proc.time()
-result_her_RDCV <- M1_repeatedDoubleCV(herring_data,  R, num_var, N_cores = 5, LassoSequence = Lassosequence, GLassoSequence = GLassosequence, n_rep=20, n_seg=3, NRSTARTS = 5)
-
-temp_lasso <- as.data.frame(result_her_RDCV$Lasso)
-temp_lasso$Var1 <- sort(as.numeric(levels(temp_lasso$Var1)))
-LASSO <- max(temp_lasso[temp_lasso[,2] == max(temp_lasso[,2]),1])  #the first max ensures that the largest Lasso value is chosen, in case more than one lasso value is recommended by M1_repeatedDoubleCV
-temp_glasso <- as.data.frame(result_her_RDCV$GroupLasso)
-temp_glasso$Var1 <- sort(as.numeric(levels(temp_glasso$Var1)))
-GLASSO <- max(temp_glasso[temp_glasso[,2] == max(temp_glasso[,2]),1])
-
-final_her_RDCV <- RegularizedSCA::sparseSCA(herring_data, num_var, R, LASSO = LASSO, GROUPLASSO = GLASSO, MaxIter = 400,
-                                            NRSTARTS = 20, method = "component")
-
-savetime_her_RdCV <- proc.time() - ptm
-final_her_RDCV$Pmatrix
-save(final_her_RDCV, savetime_her_RdCV, file="her_RdCV.RData")
-
-#3) load function M2_BIC12andIS.R 
-source("M2_BIC12andIS.R")
-
-set.seed(115)
-ptm <- proc.time()
-result_her_BICIS <- M2_BIC_IS(herring_data, num_var, R, LassoSequence = Lassosequence, GLassoSequence = GLassosequence, NRSTARTS=5)
-
-IS_index <- which(result_her_BICIS$IS == max(result_her_BICIS$IS), arr.ind = T)
-Lasso_IS <- max(Lassosequence[IS_index[1]])
-Glasso_IS <- max(GLassosequence[IS_index[2]])
-final_her_IS <- RegularizedSCA::sparseSCA(herring_data, num_var, R, LASSO = Lasso_IS, GROUPLASSO = Glasso_IS, MaxIter = 400, NRSTARTS = 20, method = "component")
-savetime_her_IS <- proc.time() - ptm
-save(final_her_IS, savetime_her_IS, file="her_IS.RData")
-
-
-#4) generate a table 
-
-load("her_RdCV.RData")  
-load("her_IS.RData")
-
-perm <- RegularizedSCA::TuckerCoef(final_her_RDCV$Tmatrix, final_her_IS$Tmatrix)$perm
-her_IS_Pmatrix <- final_her_IS$Pmatrix[, perm]   #final P matrix for IS
-
-
-set.seed(115)
-final_her_RdCV <- undoShrinkage(herring_data, R, 
-                                final_her_RDCV$Pmatrix)
-final_her_RdCV$Pmatrix
-
-final_her_IS <- undoShrinkage(herring_data, R, 
-                              her_IS_Pmatrix)  #position of components changed so as to be compared to the results by RdCV
-final_her_IS$Pmatrix
-
-final_her <- cbind(final_her_RdCV$Pmatrix, final_her_IS$Pmatrix)
-
-write.table(final_her, "final_her.csv", sep = ",")
 
 
 ######## 3. analysis of metabolomics data
